91d6008b1786692371de407e28889b94759c7732
YureiSh <enisataerkol@hotmail.com>
2025-06-29 18:01:46 +0300

    minor changes

---
+++ app/meals/error.js
@@ -0,0 +1,8 @@
+"use client"
+const Error = ({error}) => {
+    return <main className="error">
+        <h1> An error occurred!</h1>
+        <p>Failed to fetch</p>
+    </main>
+};
+export default Error;
---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+const NotFound = () => {
+    return <main className="not-found">
+        <h1> Meal Not Found!</h1>
+        <p> I think you are in the wrong URL, you should check again</p>
+
+    </main>
+};
+export default NotFound;
